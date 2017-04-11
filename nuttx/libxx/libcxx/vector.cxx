4778a3becf3c788c1f02c62d08f05b69a08ec4db
Jim Morris <morris@wolfman.com>
2017-04-10 22:33:31 -0700

    Attempt to fix the missing error in vector

---
+++ nuttx/libxx/libcxx/vector.cxx
@@ -0,0 +1,14 @@
+#include <vector>
+#include "cstdlib"
+#include "cwchar"
+#include "cerrno"
+#include "limits"
+#include "stdexcept"
+#ifdef _LIBCPP_MSVCRT
+#include "support/win32/support.h"
+#endif // _LIBCPP_MSVCRT
+#include <stdio.h>
+
+_LIBCPP_BEGIN_NAMESPACE_STD
+   template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS __vector_base_common<true>;
+_LIBCPP_END_NAMESPACE_STD
